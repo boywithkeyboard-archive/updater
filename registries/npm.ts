acdc7f50c87fed4b311590dee2bd071d9aaa411f
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-10-17 20:51:22 +0200

    feat(registries): add handler for `npm:` specifiers
    closes #18

---
+++ registries/npm.ts
@@ -0,0 +1,46 @@
+import { Registry } from '../registry.ts'
+
+export const npm = new Registry({
+  config: {
+    name: 'npm',
+    importType: 'string',
+    regex: /^npm:(@[0-9a-zA-Z.-]+\/)?[0-9a-zA-Z.-]+@[^@]+$/,
+  },
+
+  async versions(moduleName) {
+    const res = await fetch(`https://registry.npmjs.org/${moduleName}`)
+
+    if (!res.ok) {
+      return []
+    }
+
+    const { versions } = await res.json() as { versions: string[] }
+
+    return Object.keys(versions)
+  },
+
+  async repositoryUrl({ moduleName }) {
+    const res = await fetch(`https://registry.npmjs.org/${moduleName}`)
+
+    if (!res.ok) {
+      return
+    }
+
+    const { repository } = await res.json() as { repository: { url: string } }
+
+    return repository.url.replace('.git', '').replace('git+', '')
+  },
+
+  parseImport({ importString }) {
+    const moduleName = importString.substring(4) // remove npm:
+
+    const version = moduleName.startsWith('@')
+      ? moduleName.split('/')[1].split('@')[1] // scoped module
+      : moduleName.split('@')[1] // non-scoped module
+
+    return {
+      moduleName,
+      version,
+    }
+  },
+})
