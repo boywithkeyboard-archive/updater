6b3ac62a81694397b1122a5bbdff9d5a34b820e4
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2024-03-17 15:53:33 +0100

    feat(registries): add support for `denopkg.com`

--- script/registries.ts
+++ script/registries.ts
@@ -3,6 +3,7 @@ import { den_ooo_alias } from '../registries/den.ooo/alias.ts'
 import { den_ooo_gh } from '../registries/den.ooo/gh.ts'
 import { den_ooo_gl } from '../registries/den.ooo/gl.ts'
 import { deno_land } from '../registries/deno.land.ts'
+import { denopkg_com } from '../registries/denopkg.com.ts'
 import { esm_sh } from '../registries/esm.sh.ts'
 import { jsr } from '../registries/jsr.ts'
 import { npm } from '../registries/npm.ts'
@@ -18,4 +19,5 @@ export const registries = [
   den_ooo_gl,
   den_ooo_alias,
   jsr,
+  denopkg_com,
 ]
