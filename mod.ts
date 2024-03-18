c482300cdcfc31b2c3cae831bc07bd65dd2697f7
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2024-03-18 18:05:44 +0100

    undo "refactor: deprecate cli"

--- mod.ts
+++ mod.ts
@@ -1,16 +1,8 @@
-import * as colors from 'std/fmt/colors.ts'
-import { cli } from './original_cli/script/main.ts'
+import { cli } from './script/main.ts'
 
 if (import.meta.main) {
-  console.warn(
-    colors.red(
-      colors.bold('The CLI is no longer supported!') + ' ' +
-        'Please use the boywithkeyboard/updater GitHub action instead!',
-    ),
-  )
-
   cli()
 }
 
-export { checkImport } from './original_cli/script/checkImport.ts'
-export { update } from './original_cli/script/update.ts'
+export { checkImport } from './script/checkImport.ts'
+export { update } from './script/update.ts'
